0a6658ec3083017dbb1973ad47d1552be4fe97fa
Madfun2910 <Madfun2910@gmail.com>
2021-02-01 15:59:00 +0300

    new component

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {Accordion} from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
+import {OnOff} from "./components/OnOff/OnOff";
 
 
 // Function declaration
@@ -9,18 +10,18 @@ function App() {
 
     return (
             <div>
+                <OnOff on={true} />
+
                 {/*<PageTitle title={"Hello"}/>*/}
                 {/*<PageTitle title={"USER"}/>*/}
-
-                <Accordion titlevalue={"Menu"} collapsed={true}/>
-                <Accordion titlevalue={"Menu2"} collapsed={false}/>
-
-                <Rating value={0}/>
-                <Rating value={1}/>
-                <Rating value={2}/>
-                <Rating value={3}/>
-                <Rating value={4}/>
-                <Rating value={5}/>
+                {/*<Accordion titlevalue={"Menu"} collapsed={true}/>*/}
+                {/*<Accordion titlevalue={"Menu2"} collapsed={false}/>*/}
+                {/*<Rating value={0}/>*/}
+                {/*<Rating value={1}/>*/}
+                {/*<Rating value={2}/>*/}
+                {/*<Rating value={3}/>*/}
+                {/*<Rating value={4}/>*/}
+                {/*<Rating value={5}/>*/}
             </div>
     );
 }
